a43522a79c232481670c98e46df41a855d71daa1
Soutaro Matsumoto <matsumoto@soutaro.com>
2024-01-18 11:55:26 +0900

    WIP

--- lib/steep/ast/ignore.rb
+++ lib/steep/ast/ignore.rb
@@ -2,15 +2,58 @@ module Steep
   module AST
     module Ignore
       class IgnoreStart
+        attr_reader :comment, :location
 
+        def initialize(comment, location)
+          @comment = comment
+          @location = location
+        end
+
+        def line
+          location.start_line
+        end
       end
 
       class IgnoreEnd
+        attr_reader :comment, :location
 
+        def initialize(comment, location)
+          @comment = comment
+          @location = location
+        end
+
+        def line
+          location.start_line
+        end
       end
 
       class IgnoreLine
+        attr_reader :comment, :location, :raw_diagnostics
 
+        def initialize(comment, diagnostics, location)
+          @comment = comment
+          @raw_diagnostics = diagnostics
+          @location = location
+        end
+
+        def line
+          location.start_line
+        end
+
+        def ignored_diagnostics
+          if raw_diagnostics.empty?
+            return :all
+          end
+
+          if raw_diagnostics.size == 1 && raw_diagnostics[0].source == "all"
+            return :all
+          end
+
+          raw_diagnostics.map do |diagnostic|
+            name = diagnostic[:name].source
+            name.gsub(/\ARuby::/, "")
+          end
+        end
       end
 
       def self.parse(comment, buffer)
@@ -26,9 +69,10 @@ module Steep
 
         case
         when scanner.scan(/steep:ignore:start\b/)
+          matched = scanner.matched or raise
           loc = RBS::Location.new(
             buffer,
-            begin_pos + scanner.charpos - scanner.size,
+            begin_pos + scanner.charpos - matched.size,
             begin_pos + scanner.charpos
           )
 
@@ -37,9 +81,10 @@ module Steep
 
           IgnoreStart.new(comment, loc)
         when scanner.scan(/steep:ignore:end\b/)
+          matched = scanner.matched or raise
           loc = RBS::Location.new(
             buffer,
-            begin_pos + scanner.charpos - scanner.size,
+            begin_pos + scanner.charpos - matched.size,
             begin_pos + scanner.charpos
           )
 
@@ -51,14 +96,43 @@ module Steep
           # @type var diagnostics: IgnoreLine::diagnostics
           diagnostics = []
 
-          keyword_begin = begin_pos + scanner.charpos - scanner.size
-          keyword_end = begin_pos + scanner.charpos
+          keyword_match = scanner.matched or raise
+          loc = RBS::Location.new(
+            buffer,
+            begin_pos + scanner.charpos - keyword_match.size,
+            begin_pos + scanner.charpos
+          )
 
           until (scanner.skip(/\s*/); scanner.eos?)
-            if scanner.scan(/all\b/)
+            if scanner.scan(/[A-Z][\w]*/)
+              name = scanner.matched or raise
+              name_begin = begin_pos + scanner.charpos - name.size
+              name_end = begin_pos + scanner.charpos
+            else
+              break
+            end
 
+            scanner.skip(/\s*/)
+            if scanner.scan(/,/)
+              comma_begin = begin_pos + scanner.charpos - 1
+              comma_end = begin_pos + scanner.charpos
+            end
+
+            if comma_begin && comma_end
+              loc = RBS::Location.new(buffer, name_begin, comma_end)
+
+            else
+              break
             end
           end
+
+          IgnoreLine.new(comment, diagnostics, loc)
+
+        #   until (scanner.skip(/\s*/); scanner.eos?)
+        #     if scanner.scan(/all\b/)
+
+        #     end
+        #   end
 
           # if list = $3
           #   list.strip!
@@ -70,7 +144,7 @@ module Steep
           #   end
           # end
 
-          IgnoreLine.new(comment, diagnostics, loc)
+          # IgnoreLine.new(comment, diagnostics, loc)
         end
       end
     end
